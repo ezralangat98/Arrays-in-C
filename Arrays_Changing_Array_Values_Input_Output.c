6df65f3cb52995267cc3b7f35ba8928daed2b3aa
Ezra <ezralangat88@gmail.com>
2021-10-05 15:50:25 -0700

    Commit at:05102021_1550

--- Arrays_Changing_Array_Values_Input_Output.c
+++ Arrays_Changing_Array_Values_Input_Output.c
@@ -5,16 +5,28 @@ int main()
 {
   //initializing an array during declaration
   int mark[5] = {40, 60, 80, 44, 90};
+   //initializing an 2D array during declaration
+  int two_dimensional_arr[2][4]={ {20,5,6,70},
+                                  {30,8,9,10}};
 
   //Changing the value in the first element
   mark[0] = 50;
   //Initializing arrays without stating no of elements
   int age[] = {8, 17, 9};
 
-  int addresscode[2];
+  //Declaring and initializing strings using arrays
+   char name[6] = {'G', 'E', 'E', 'K', 'S', '\0'};
 
+  int addresscode[2];
+  
+  printf("Hello: %s\n", name );
+  
   printf("Marks in mark[0] is: %d", mark[0]);
   printf("\nAge of the first person is: %d", age[0]);
+
+  printf("\nFor Two dimension array the value at row 1, column 3 is:\n %d", 
+  two_dimensional_arr[0][2]);
+
   //Input and Output in Arrays
   printf("\nEnter the new age of the third person: ");
   // taking input and storing it in the 3rd element of age to replace previous age
@@ -34,6 +46,7 @@ int main()
   for(int i = 0; i < 2; ++i) {
      printf("%d\n", addresscode[i]);
   }
+  
   
   return 0;
   
